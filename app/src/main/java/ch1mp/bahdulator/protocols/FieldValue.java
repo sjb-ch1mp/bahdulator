b472e85621c862ad86de9bc189ff66ff4098a51d
sjb-ch1mp <samuel.brookes@hotmail.com>
2019-12-20 12:16:02 +1100

    added protocol data

--- app/src/main/java/ch1mp/bahdulator/protocols/FieldValue.java
+++ app/src/main/java/ch1mp/bahdulator/protocols/FieldValue.java
@@ -2,6 +2,12 @@ package ch1mp.bahdulator.protocols;
 
 public class FieldValue {
     int key;
-    String identifier;
+    String keyWord;
     String description;
+
+    public FieldValue(int key, String keyWord, String description){
+        this.key = key;
+        this.keyWord = keyWord;
+        this.description = description;
+    }
 }
